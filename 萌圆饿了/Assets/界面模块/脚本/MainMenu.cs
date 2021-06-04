c71066182d5982444986c93058207cb51def51b2
WheatDew <wheatdew@163.com>
2021-06-04 22:43:56 +0800

    主菜单脚本

---
+++ 萌圆饿了/Assets/界面模块/脚本/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject[] submenuList;
+
+    private void Start()
+    {
+
+    }
+
+    public void SetSubmenu(int id)
+    {
+
+        if (!submenuList[id].activeSelf)
+        {
+            foreach (var item in submenuList)
+            {
+                item.SetActive(false);
+            }
+            submenuList[id].SetActive(true);
+        }
+        else
+        {
+            foreach (var item in submenuList)
+            {
+                item.SetActive(false);
+            }
+        }
+
+
+    }
+
+}
