51150a241545c8d3fc3c80a027b5502085cd2da7
WheatDew <wheatdew@163.com>
2021-06-06 03:34:29 +0800

    替换捏人图片功能

---
+++ 萌圆饿了/Assets/界面模块/脚本/CharacterBodySpriteSourceButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterBodySpriteSourceButton : MonoBehaviour
+{
+    public string characterBodyPartName;
+    public Sprite spriteSource;
+    void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(delegate
+        {
+            FindObjectOfType<CharacterBodySpriteSourceController>().CharacterBodySpriteList[characterBodyPartName].sprite = spriteSource;
+        });
+    }
+
+}
---
+++ 萌圆饿了/Assets/界面模块/脚本/CharacterBodySpriteSourceController.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBodySpriteSourceController : MonoBehaviour
+{
+    public Dictionary<string, SpriteRenderer> CharacterBodySpriteList = new Dictionary<string, SpriteRenderer>();
+
+
+}
