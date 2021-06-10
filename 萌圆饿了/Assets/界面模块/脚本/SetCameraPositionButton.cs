5a27a224b844a49bfffc4fa204d46e83b7312f01
WheatDew <wheatdew@163.com>
2021-06-11 00:56:48 +0800

    回退

--- 萌圆饿了/Assets/界面模块/脚本/SetCameraPositionButton.cs
+++ 萌圆饿了/Assets/界面模块/脚本/SetCameraPositionButton.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 public class SetCameraPositionButton : MonoBehaviour
 {
     public Vector3 position;
-    public Vector3 rotation;
 
     private void Start()
     {
         GetComponent<UnityEngine.UI.Button>().onClick.AddListener(delegate
         {
             Camera.main.gameObject.transform.position = position;
-            Camera.main.gameObject.transform.rotation = Quaternion.Euler(rotation);
         });
     }
 }
