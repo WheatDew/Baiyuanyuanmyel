d36bd84ecca3ee61b0a0fe51cb7d9e14458c8ac8
WheatDew <wheatdew@163.com>
2021-06-23 18:36:55 +0800

    对话和语音
    对话功能和语音功能的前置

---
+++ 萌圆饿了/Assets/功能试验模块/对话框功能/DialogueController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueController : MonoBehaviour
+{
+    //需要场景中有对应的ui
+    public DialogueComponent c_Dialogue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        c_Dialogue.characterName.text = "";
+        c_Dialogue.content.text = "";
+        c_Dialogue.gameObject.SetActive(false);
+    }
+
+    public void SetDialogueSingle(string content,string characterName="")
+    {
+        c_Dialogue.content.text = content;
+        c_Dialogue.characterName.text = characterName;
+    }
+}
---
+++ 萌圆饿了/Assets/功能试验模块/语音控制功能/VoiceController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VoiceController : MonoBehaviour
+{
+    public GameObject[] voiceElement;
+
+    private void Update()
+    {
+        RaycastHit result;
+        Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition),out result);
+        if (result.collider.tag == "character")
+        {
+            
+        }
+    }
+
+}
