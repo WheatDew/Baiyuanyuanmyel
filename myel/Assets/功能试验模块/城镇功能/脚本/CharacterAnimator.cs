32e5e3bb16a538775fb102223f0145235f8a168a
WheatDew <wheatdew@163.com>
2021-07-11 18:28:42 +0800

    城镇功能动画

---
+++ myel/Assets/功能试验模块/城镇功能/脚本/CharacterAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    public MeshRenderer m_meshRenderer;
+    private CharacterTextureList characterTextureList;
+    public int currentTexture = 0;
+
+    private void Start()
+    {
+        characterTextureList = FindObjectOfType<CharacterTextureList>();
+        m_meshRenderer.material.mainTexture = characterTextureList.textureList[currentTexture];
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UpdateCurrentTexture(currentTexture);
+        }
+    }
+
+    public void UpdateCurrentTexture(int textureID)
+    {
+        m_meshRenderer.material.mainTexture = characterTextureList.textureList[textureID];
+    }
+}
