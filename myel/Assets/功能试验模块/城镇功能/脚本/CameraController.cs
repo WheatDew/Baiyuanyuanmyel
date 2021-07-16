ab1e3a35aa9fb53d100d73cb73c1073ff6fe1b5f
WheatDew <wheatdew@163.com>
2021-07-16 10:45:48 +0800

    人物摄像机及动画

---
+++ myel/Assets/功能试验模块/城镇功能/脚本/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private AxisCamera a_camera ;
+    private Transform m_camera;
+    public Vector3 min;
+    public Vector3 max;
+    [Range(0,1)]
+    public float currentHight=0;
+
+    private void Start()
+    {
+        a_camera = FindObjectOfType<AxisCamera>();
+        m_camera = a_camera.transform.GetChild(0);
+    }
+
+    private void Update()
+    {
+        currentHight -= Input.GetAxis("Mouse ScrollWheel")*0.1f;
+        if (currentHight < 0)
+            currentHight = 0;
+        if (currentHight > 1)
+            currentHight = 1;
+        m_camera.localPosition = Vector3.Lerp(min, max, currentHight);
+        m_camera.localRotation = Quaternion.AngleAxis(20 * currentHight, Vector3.right);
+        if (Input.GetMouseButton(2))
+        {
+            a_camera.transform.position += new Vector3(Input.GetAxis("Mouse X"), 0, Input.GetAxisRaw("Mouse Y"));
+        }
+    }
+}
--- myel/Assets/功能试验模块/城镇功能/脚本/CharacterAnimator.cs
+++ myel/Assets/功能试验模块/城镇功能/脚本/CharacterAnimator.cs
@@ -6,26 +6,50 @@ public class CharacterAnimator : MonoBehaviour
 {
     
     public MeshRenderer m_meshRenderer;
-    private CharacterTextureList characterTextureList;
-    public int currentTexture = 0;
+    public Rigidbody m_rigibody;
+    public int currentStatu = 0;
+    public int[] currentAnimation;
+    public bool isPlaying;
 
     private void Start()
     {
-        characterTextureList = FindObjectOfType<CharacterTextureList>();
-        m_meshRenderer.material.mainTexture = characterTextureList.textureList[currentTexture].texture;
+        currentAnimation = new int[] { 0, 4, 8, 12 };
+        StartCoroutine(PlayAnimation());
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            UpdateCurrentTexture(currentTexture);
-        }
+        if (m_rigibody.velocity.z > 0)
+            currentAnimation = new int[] { 1, 6, 11, 16 };
+        if (m_rigibody.velocity.z < 0)
+            currentAnimation = new int[] { 2, 7, 12, 17 };
+        if (m_rigibody.velocity.x < 0)
+            currentAnimation = new int[] { 3, 8, 13, 18 };
+        if (m_rigibody.velocity.x > 0)
+            currentAnimation = new int[] { 4, 9, 14, 19 };
+        if (m_rigibody.velocity==Vector3.zero)
+            currentAnimation = new int[] { 0, 5, 10, 15 };
     }
 
-    public void UpdateCurrentTexture(int textureID)
+    public void SetCurrentStatu(int id)
     {
-        m_meshRenderer.material.mainTexture = characterTextureList.textureList[textureID].texture;
-        
+        m_meshRenderer.material.mainTextureOffset = new Vector2((id / 5)*0.25f, (id % 5)*0.2f);
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        int currentAnimationID = 0;
+        while (true)
+        {
+            if (isPlaying && currentAnimation.Length != 0)
+            {
+                SetCurrentStatu(currentAnimation[currentAnimationID]);
+                currentAnimationID++;
+                if (currentAnimationID > currentAnimation.Length - 1)
+                    currentAnimationID = 0;
+            }
+            
+            yield return new WaitForSeconds(0.25f);
+        }
     }
 }
