e279de6c66cf922ce8ea9d0ad87d497f20fbbb0a
GenIndex <1370087719@qq.com>
2021-07-16 12:39:19 +0800

    步行街

---
+++ myel/Assets/功能试验模块/城镇功能/脚本/CameraController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Camera m_camera;
+
+    private void Update()
+    {
+            
+    }
+}
---
+++ myel/Assets/功能试验模块/城镇功能/脚本/CharacterAnimator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+    
+    public MeshRenderer m_meshRenderer;
+    private CharacterTextureList characterTextureList;
+    public int currentTexture = 0;
+
+    private void Start()
+    {
+        characterTextureList = FindObjectOfType<CharacterTextureList>();
+        m_meshRenderer.material.mainTexture = characterTextureList.textureList[currentTexture].texture;
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            UpdateCurrentTexture(currentTexture);
+        }
+    }
+
+    public void UpdateCurrentTexture(int textureID)
+    {
+        m_meshRenderer.material.mainTexture = characterTextureList.textureList[textureID].texture;
+        
+    }
+}
