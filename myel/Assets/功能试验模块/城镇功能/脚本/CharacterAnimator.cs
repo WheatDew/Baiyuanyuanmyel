715ec0a5153aef90c892dff1f13ad894b6b78dca
WheatDew <wheatdew@163.com>
2021-07-17 15:47:51 +0800

    动画和人物重做

--- myel/Assets/功能试验模块/城镇功能/脚本/CharacterAnimator.cs
+++ myel/Assets/功能试验模块/城镇功能/脚本/CharacterAnimator.cs
@@ -6,26 +6,56 @@ public class CharacterAnimator : MonoBehaviour
 {
     
     public MeshRenderer m_meshRenderer;
-    private CharacterTextureList characterTextureList;
-    public int currentTexture = 0;
+    public Rigidbody m_rigidbody;
+    public int[] animationArray;
 
-    private void Start()
+    public void Start()
     {
-        characterTextureList = FindObjectOfType<CharacterTextureList>();
-        m_meshRenderer.material.mainTexture = characterTextureList.textureList[currentTexture].texture;
+        StartCoroutine(PlayAnimation());
     }
 
     public void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if(Mathf.Abs(m_rigidbody.velocity.x)<Mathf.Abs(m_rigidbody.velocity.z))
         {
-            UpdateCurrentTexture(currentTexture);
+            if (m_rigidbody.velocity.z > 0)
+                animationArray = new int[] { 1, 6, 11, 16 };
+            if (m_rigidbody.velocity.z < 0)
+                animationArray = new int[] { 2, 7, 12, 17 };
+        }
+        else
+        {
+            if (m_rigidbody.velocity.x < 0)
+                animationArray = new int[] { 3, 8, 13, 18 };
+            if (m_rigidbody.velocity.x > 0)
+                animationArray = new int[] { 4, 9, 14, 19 };
+        }
+        
+        
+        if (m_rigidbody.velocity==Vector3.zero)
+            animationArray = new int[] { 0, 5, 10, 15 };
+    }
+
+    IEnumerator PlayAnimation()
+    {
+        int currentID = 0;
+        while (true)
+        {
+            if (animationArray.Length != 0)
+            {
+
+                SetCharacterTexture(animationArray[currentID]);
+                currentID++;
+                if (currentID > 3)
+                    currentID = 0;
+
+            }
+            yield return new WaitForSeconds(0.25f);
         }
     }
 
-    public void UpdateCurrentTexture(int textureID)
+    public void SetCharacterTexture(int id)
     {
-        m_meshRenderer.material.mainTexture = characterTextureList.textureList[textureID].texture;
-        
+        m_meshRenderer.material.mainTextureOffset = new Vector2((id / 5) * 0.25f, (id % 5) * 0.2f);
     }
 }
