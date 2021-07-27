21c91d1be3c635998354fe4733bcf79b10c745f7
WheatDew <wheatdew@163.com>
2021-07-27 19:22:31 +0800

    角色闲逛

--- myel/Assets/城镇游戏主体/脚本/角色控制界面/CharacterAnimator.cs
+++ myel/Assets/城镇游戏主体/脚本/角色控制界面/CharacterAnimator.cs
@@ -4,36 +4,71 @@ using UnityEngine;
 
 public class CharacterAnimator : MonoBehaviour
 {
-    
+    public CharacterNavigation characterNavigation;
+    private CharacterSelectionController selectionController;
     public MeshRenderer m_meshRenderer;
     public Rigidbody m_rigidbody;
     public int[] animationArray;
+    public bool isNav=true;
 
     public void Start()
     {
+        selectionController = FindObjectOfType<CharacterSelectionController>();
         StartCoroutine(PlayAnimation());
     }
 
     public void Update()
     {
-        if(Mathf.Abs(m_rigidbody.velocity.x)<Mathf.Abs(m_rigidbody.velocity.z))
+        
+
+        if((selectionController.character == null || selectionController.character != gameObject))
         {
-            if (m_rigidbody.velocity.z > 0)
-                animationArray = new int[] { 1, 6, 11, 16 };
-            if (m_rigidbody.velocity.z < 0)
-                animationArray = new int[] { 2, 7, 12, 17 };
+            isNav = true;
+            if (Mathf.Abs(characterNavigation.targetPosition.x - transform.position.x)
+            < Mathf.Abs(characterNavigation.targetPosition.z - transform.position.z))
+            {
+                if (characterNavigation.targetPosition.z - transform.position.z > 0)
+                    animationArray = new int[] { 1, 6, 11, 16 };
+                if (characterNavigation.targetPosition.z - transform.position.z < 0)
+                    animationArray = new int[] { 2, 7, 12, 17 };
+            }
+            else
+            {
+                if (characterNavigation.targetPosition.x - transform.position.x < 0)
+                    animationArray = new int[] { 3, 8, 13, 18 };
+                if (characterNavigation.targetPosition.x - transform.position.x > 0)
+                    animationArray = new int[] { 4, 9, 14, 19 };
+            }
+
+
+            if (Mathf.Abs(characterNavigation.targetPosition.x - transform.position.x) < 0.05f
+                && Mathf.Abs(characterNavigation.targetPosition.z - transform.position.z) < 0.05f)
+                animationArray = new int[] { 0, 5, 10, 15 };
         }
         else
         {
-            if (m_rigidbody.velocity.x < 0)
-                animationArray = new int[] { 3, 8, 13, 18 };
-            if (m_rigidbody.velocity.x > 0)
-                animationArray = new int[] { 4, 9, 14, 19 };
+            isNav = false;
+            if (Mathf.Abs(m_rigidbody.velocity.x) < Mathf.Abs(m_rigidbody.velocity.z))
+            {
+                if (m_rigidbody.velocity.z > 0)
+                    animationArray = new int[] { 1, 6, 11, 16 };
+                if (m_rigidbody.velocity.z < 0)
+                    animationArray = new int[] { 2, 7, 12, 17 };
+            }
+            else
+            {
+                if (m_rigidbody.velocity.x < 0)
+                    animationArray = new int[] { 3, 8, 13, 18 };
+                if (m_rigidbody.velocity.x > 0)
+                    animationArray = new int[] { 4, 9, 14, 19 };
+            }
+
+
+            if (Mathf.Abs(m_rigidbody.velocity.x)<0.05f&& Mathf.Abs(m_rigidbody.velocity.z)<0.05f)
+                animationArray = new int[] { 0, 5, 10, 15 };
         }
+
         
-        
-        if (m_rigidbody.velocity==Vector3.zero)
-            animationArray = new int[] { 0, 5, 10, 15 };
     }
 
     IEnumerator PlayAnimation()
---
+++ myel/Assets/城镇游戏主体/脚本/角色控制界面/CharacterNavigation.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class CharacterNavigation : MonoBehaviour
+{
+    private NavMeshAgent navMeshAgent;
+    private CharacterSelectionController selectionController;
+    public Vector3 targetPosition;
+
+    private void Start()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        selectionController = FindObjectOfType<CharacterSelectionController>();
+        StartCoroutine(SetStroll());
+    }
+
+    public void Update()
+    {
+        if(selectionController.character == gameObject)
+        {
+            navMeshAgent.isStopped = false;
+            navMeshAgent.SetDestination(transform.position);
+        }
+    }
+
+    IEnumerator SetStroll()
+    {
+        targetPosition = transform.position;
+        while (true)
+        {
+            if ((selectionController.character==null||selectionController.character != gameObject)
+                && (Mathf.Abs(targetPosition.x - transform.position.x) < 0.01f
+                && Mathf.Abs(targetPosition.z - transform.position.z) < 0.01f))
+            {
+                navMeshAgent.isStopped = false;
+                targetPosition = new Vector3(transform.position.x + Random.Range(-10f, 10f),
+                    0, transform.position.z + Random.Range(-10f, 10f));
+                navMeshAgent.SetDestination(targetPosition);
+            }
+            else
+            {
+                navMeshAgent.isStopped = true;
+            }
+
+            yield return new WaitForSeconds(3);
+        }
+    }
+}
