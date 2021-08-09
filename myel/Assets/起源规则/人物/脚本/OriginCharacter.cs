424c790e243225e159ce8c64e827747800f8d552
WheatDew <wheatdew@163.com>
2021-08-10 00:58:10 +0800

    替换小希的贴图

--- myel/Assets/起源规则/人物/脚本/OriginCharacter.cs
+++ myel/Assets/起源规则/人物/脚本/OriginCharacter.cs
@@ -244,5 +244,67 @@ public class OriginCharacter : MonoBehaviour
     }
 
     #endregion
+
+    #region 角色动作模块
+
+    //public Rigidbody m_rigidbody;
+    //public float multiple;
+    //public float jumpMultiple;
+    //public bool isGround = false;
+    //public string groundName;
+
+    //private CharacterSelectionController selectionController;
+
+    //private void Start()
+    //{
+    //    selectionController = FindObjectOfType<CharacterSelectionController>();
+    //}
+
+
+    //private void Update()
+    //{
+    //    if (selectionController.character == gameObject)
+    //    {
+    //        m_rigidbody.AddForce(Input.GetAxis("Horizontal") * multiple, 0, Input.GetAxis("Vertical") * multiple);
+
+    //        if (isGround && Input.GetKeyDown(KeyCode.Space))
+    //        {
+    //            m_rigidbody.AddForce(Vector3.up * jumpMultiple);
+
+    //        }
+
+    //        if (!isGround)
+    //            m_rigidbody.AddForce(Vector3.down * jumpMultiple * 0.01f);
+    //    }
+    //}
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Ground")
+    //    {
+    //        isGround = true;
+    //    }
+    //}
+
+    //private void OnCollisionExit(Collision collision)
+    //{
+    //    if (collision.gameObject.tag == "Ground")
+    //    {
+    //        isGround = false;
+    //    }
+    //}
+
+
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    groundName = other.gameObject.name;
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    groundName = "";
+    //}
+
+    #endregion
 }
 
